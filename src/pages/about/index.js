2023e110999f281369b547523d7acb32e8b07a8b
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2022-01-23 20:16:31 +0000

    add work about section

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -14,6 +14,11 @@ const AboutPage = () => {
         </div>
         <StaticImage src={'../../images/avatar.png'} alt={'Avatar image'} className={styles.avatar}/>
       </div>
+      <div className={styles.workContainer}>
+      <h4>Trabajo</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+      <h4>Bio</h4>
       <BioSection year={'1992'} paragraph={'Nacido en La Palma (Canarias).'}/>
       <BioSection year={'2020'} paragraph={'Finalización de ciclo superior de Desarrollo de Aplicaciones Multiplataforma.'} tags={['Java', 'SQL', 'CSS3', 'HTML5', 'OOP', 'SOLID']}/>
       <BioSection year={'2020'} paragraph={'Curso Bootcamp Full Stack Developer EOI.'} tags={['NodeJS', 'JavaScript', 'CSS3', 'HTML5', 'VueJS', 'React', 'Jest', 'Testing Library', 'Go', 'SQL', 'GIT', 'Docker']}/>
