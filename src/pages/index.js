5a9411461d3a90011ea75d36ee7b14f706ee7284
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2022-01-30 12:03:40 +0000

    change animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,9 @@ import { useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 
 const variants = {
-  hidden: { opacity: 0, x: -20, y: 0 },
+  hidden: { opacity: 0, x: 0, y: 20 },
   enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 20, y: 0 }
+  exit: { opacity: 0, x: -0, y: 20 }
 }
 
 const AboutPage = ({ data }) => {
