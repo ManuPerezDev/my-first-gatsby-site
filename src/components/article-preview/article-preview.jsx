6620be7447f4a54b58b632c6f88fbd26c41d2c88
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2022-09-02 12:38:26 +0100

    update article preview component

--- src/components/article-preview/article-preview.jsx
+++ src/components/article-preview/article-preview.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as styles from './article-preview.module.css'
 import { Link } from 'gatsby'
-import { motion } from 'framer-motion'
 
 const ArticlePreview = ({ posts }) => (
   <section className={styles.articlePreviewContainer}>
     {posts.map((post) => {
       return (
         <Link to={`/blog/${post.slug}`}>
-          <div className={styles.title}>{post.frontmatter.title}</div>
-          <motion.div
-            whileHover={{ scale: 1.2 }}
-          >
+          <div className={styles.postInfoContainer}>
+            <div className={styles.postInfo}>
               <GatsbyImage
-              image={getImage(post.frontmatter.hero_image)}
-              alt={post.frontmatter.hero_image_alt}
-              className={styles.image}
-            />
-          </motion.div>
+                image={getImage(post.frontmatter.hero_image)}
+                alt={post.frontmatter.hero_image_alt}
+                className={styles.image}
+              />
+              <div className={styles.titleDescriptionContainer}>
+                <div className={styles.title}>{post.frontmatter.title}</div>
+                <div className={styles.description}>
+                  {post.frontmatter.description}
+                </div>
+              </div>
+            </div>
+          </div>
         </Link>
       )
     })
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
-import * as styles from './about/main-page.module.css'
-import BioSection from '../components/bio-section/bio-section'
 import { graphql } from 'gatsby'
 import ArticlePreview from '../components/article-preview/article-preview'
-import * as en from '../locales/en.json'
-import * as es from '../locales/es.json'
-import { useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-
-const variants = {
-  hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: -0, y: 20 }
-}
 
 const AboutPage = ({ data }) => {
   const posts = data.allMdx.nodes
