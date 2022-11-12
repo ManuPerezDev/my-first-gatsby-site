44e54ecd086bb5a86dd04f72e386265425c3f6f0
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2022-11-12 11:59:26 +0000

    rename collaborator component to information

--- src/components/bio-section/bio-section.js
+++ src/components/bio-section/bio-section.js
@@ -14,37 +14,27 @@ const BioSection = ({ year, paragraph, tags, collaborations }) => {
     <div onClick={handleDisplayCollaborations} className={styles.bioSectionWrapper}>
       <div className={styles.bioSection}>
         <span>{year}</span>
-        <div>
-          <p>
-            {paragraph}
-          </p>
-          {tags.length > 0 && <div className={styles.tagContainer}>
-            {tags.map(tag => <Tag name={tag}/>)}
-          </div>
-          }
-        </div>
+        <Information description={paragraph} tags={tags}/>
         {collaborations && (displayCollaborations ? <AiFillCaretDown style={{ width: '2em' }}/> : <AiFillCaretRight style={{ width: '2em' }}/>)}
       </div>
       {collaborations && <div className={styles.collaborationsWrapper} style={{ display: displayCollaborations ? 'inline-block' : 'none' }}>
-        {<div className={styles.collaborations}>{collaborations.map(collaboration => <Collaboration
+        {<div className={styles.collaborations}>{collaborations.map(collaboration => <Information
           description={collaboration.description} tags={collaboration.tools}/>)}</div>}
       </div>}
     </div>
   )
 }
 
-const Collaboration = ({ description, tags }) => {
+const Information = ({ description, tags }) => {
   return (
-    <div className={styles.bioSection}>
-      <div>
-        <p>
-          {description}
-        </p>
-        {tags && <div className={styles.tagContainer}>
-          {tags.map(tag => <Tag name={tag}/>)}
-        </div>
-        }
+    <div>
+      <p>
+        {description}
+      </p>
+      {tags.length > 0 && <div className={styles.tagContainer}>
+      {tags.map(tag => <Tag name={tag}/>)}
       </div>
+      }
     </div>
   )
 }
