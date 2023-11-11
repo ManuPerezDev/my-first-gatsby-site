d17627939ed3791ab331c2578df062a8b5e8a57c
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2023-11-11 12:28:57 +0000

    remove main image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 import ArticlePreview from '../components/article-preview/article-preview'
-import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = ({ data }) => {
   const posts = data.allMarkdownRemark.nodes
 
   return (
     <Layout>
-      <StaticImage src={'../../static/images/pandas.jpeg'} alt={'A group of pandas.'}/>
       <ArticlePreview posts={posts}/>
     </Layout>
   )
