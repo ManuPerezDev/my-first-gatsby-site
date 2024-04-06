d59cac186b66c021aa19506471a1dbca4ee8a8d3
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2024-04-06 16:25:14 +0100

    refactor experience section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,47 +2,35 @@ import React from 'react'
 import * as styles from '../Experience/Experience.module.css'
 import { Button, Timeline } from 'flowbite-react'
 import { HiArrowNarrowRight, HiCalendar } from 'react-icons/hi'
+import { experienceInfo } from './ExperienceInfo'
 
 export const Experience = () => {
   return (
     <section id='experience'>
       <h2>Experiencia</h2>
       <Timeline className={styles.timeline}>
-        <Timeline.Item>
-          <Timeline.Point icon={HiCalendar}/>
-          <Timeline.Content>
-            <Timeline.Title className={styles.title}>Desarrollador de Software - <span className={styles.organization}>Lean Mind</span></Timeline.Title>
-            <Timeline.Time>Marzo 2021</Timeline.Time>
-            <Timeline.Body className={styles.body}>
-              En 2021 tuve el honor de ser contratado en el equipo de Carlos Blé, una empresa de desarrollo de software y consultoría.
-              En Lean Mind he tenido la oportunidad de trabajar y aprender en equipos de alto rendimiento enfocados en metodologías Agile, especialmente Extreme Programming.
-            </Timeline.Body>
-              <Button className={styles.button} as={'a'} href={'https://www.leanmind.es/es/'} color='gray'>
-                Learn More
-                <HiArrowNarrowRight className='ml-2 h-3 w-3' />
-              </Button>
-          </Timeline.Content>
-        </Timeline.Item>
-        <Timeline.Item>
-          <Timeline.Point icon={HiCalendar} />
-          <Timeline.Content>
-            <Timeline.Title className={styles.title}>Estudiante Bootcamp Fullstack - <span className={styles.organization}>Escuela de Organización Industrial</span></Timeline.Title>
-            <Timeline.Time>Septiembre 2020</Timeline.Time>
-            <Timeline.Body className={styles.body}>
-              Inscripción en la <a href='https://www.eoi.es/es/cursos/90441/curso-de-programacion-fullstack-presencia-virtual-online-tenerife-5a-edicion'>5ª edición</a> del Curso de Programación Fullstack de la EOI dirigido por <a href='https://manz.dev/'>ManzDev</a>.
-            </Timeline.Body>
-          </Timeline.Content>
-        </Timeline.Item>
-        <Timeline.Item>
-          <Timeline.Point icon={HiCalendar} />
-          <Timeline.Content>
-            <Timeline.Title className={styles.title}>Estudiante Ciclo Superior - <span className={styles.organization}>CIFP Cesar Manrique</span></Timeline.Title>
-            <Timeline.Time>Septiembre 2018</Timeline.Time>
-            <Timeline.Body className={styles.body}>
-              Estudio de DAM en el CIFP Cesar Manrique, Santa Cruz de Tenerife.
-            </Timeline.Body>
-          </Timeline.Content>
-        </Timeline.Item>
+        {
+          experienceInfo().map((experience, index) => {
+            return (
+              <Timeline.Item key={index}>
+                <Timeline.Point icon={HiCalendar}/>
+                <Timeline.Content>
+                  <Timeline.Title className={styles.title}>{experience.title}</Timeline.Title>
+                  <Timeline.Time>{experience.time}</Timeline.Time>
+                  <Timeline.Body className={styles.body}>
+                    {experience.body}
+                  </Timeline.Body>
+                  {experience.learnMoreLink &&
+                  <Button className={styles.button} as={'a'} href={experience.learnMoreLink} color='gray'>
+                    Learn More
+                    <HiArrowNarrowRight className='ml-2 h-3 w-3' />
+                  </Button>
+                  }
+                </Timeline.Content>
+              </Timeline.Item>
+            )
+          })
+        }
       </Timeline>
     </section>
   )
---
+++ src/components/Experience/ExperienceInfo.js
@@ -0,0 +1,24 @@
+export const experienceInfo = () => {
+  return [
+    {
+      title: 'Desarrollador de Software',
+      time: 'Marzo 2021',
+      organization: 'Lean Mind',
+      body: 'En 2021 tuve el honor de ser contratado en el equipo de Carlos Blé, una empresa de desarrollo de software y consultoría. En Lean Mind he tenido la oportunidad de trabajar y aprender en equipos de alto rendimiento enfocados en metodologías Agile, especialmente Extreme Programming.',
+      learnMoreLink: 'https://www.leanmind.es/es/'
+    },
+    {
+      title: 'Estudiante Bootcamp Fullstack',
+      time: 'Septiembre 2020',
+      organization: 'Escuela de Organización Industrial',
+      body: 'Inscripción en la 5ª edición del Curso de Programación Fullstack de la EOI dirigido por ManzDev.',
+      learnMoreLink: 'https://www.eoi.es/es/cursos/90441/curso-de-programacion-fullstack-presencia-virtual-online-tenerife-5a-edicion'
+    },
+    {
+      title: 'Estudiante Ciclo Superior',
+      time: 'Septiembre 2018',
+      organization: 'CIFP Cesar Manrique',
+      body: 'Estudio de DAM en el CIFP Cesar Manrique, Santa Cruz de Tenerife.'
+    }
+  ]
+}
