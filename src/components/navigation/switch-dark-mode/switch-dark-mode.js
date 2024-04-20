14653ec6a2ff2cec360a2fef8f40eb0d37d47e85
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2024-04-20 14:02:19 +0100

    remove detect default theme

--- src/components/navigation/switch-dark-mode/switch-dark-mode.js
+++ src/components/navigation/switch-dark-mode/switch-dark-mode.js
@@ -6,8 +6,7 @@ import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs'
 
 const Switch = () => {
   const isBrowser = () => typeof window !== 'undefined'
-  const defaultDark = isBrowser() ? window.matchMedia('(prefers-color-scheme: dark)').matches : true
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light')
+  const [theme, setTheme] = useLocalStorage('theme', 'dark')
 
   const switchTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light'
