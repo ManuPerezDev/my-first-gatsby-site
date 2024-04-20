08cf07d59c3cbc5e78fcec798a81e495a96e1021
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2024-04-20 13:28:34 +0100

    fix import

--- src/components/Stack/Icons/PairProgramming/PairProgramming.jsx
+++ src/components/Stack/Icons/PairProgramming/PairProgramming.jsx
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 import pairProgramming from './Pairprogramming.png'
 
 export const PairProgramming = props => <img width={48} height={48} {...props} src={pairProgramming} alt='programming'/>
